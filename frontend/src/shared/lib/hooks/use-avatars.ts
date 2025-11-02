4be46de73388aa2f9c7874e41f6dea260b9e8206
letnull19A <alexvolkovdd@googlemail.com>
2025-11-03 02:24:47 +0400

    feat(ui): Added useAvatarsSuspense

--- frontend/src/shared/lib/hooks/use-avatars.ts
+++ frontend/src/shared/lib/hooks/use-avatars.ts
@@ -1,11 +1,17 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { AvatarApi } from '../../api/avatar';
-import type { avatarTypes } from '@/shared/api';
 
-export const useAvatars = (params: avatarTypes.ListAvatarsParams) => {
+export const useAvatars = (params: { pick: number; offset: number }) => {
   return useQuery({
     queryKey: ['avatars', params],
     queryFn: () => AvatarApi.getAll(params.pick, params.offset),
     placeholderData: keepPreviousData,
+  });
+};
+
+export const useAvatarsSuspense = (params: { pick: number; offset: number }) => {
+  return useSuspenseQuery({
+    queryKey: ['avatars', params],
+    queryFn: () => AvatarApi.getAll(params.pick, params.offset),
   });
 };
