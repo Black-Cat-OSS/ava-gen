cdd6c15d5d462311c90a1a515a50b7db6cbcead0
letnull19A <alexvolkovdd@googlemail.com>
2025-11-04 03:16:06 +0400

    refactor: Renamed BaseGenerateParams to IColorScheme

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -2,3 +2,9 @@ export { AvatarApi } from './avatar';
 export { GeneratorApi } from './generator';
 export { PalleteApi } from './pallete';
 export type { ApiPagination, ApiRequest } from './api';
+export type {
+  GenerateAvatarResponse,
+  GenerateAvatarProcedural,
+  GenerateAvatarAngular,
+} from './generator/types';
+export type { GenerateEmojiAvatarParams } from './generator/types';
