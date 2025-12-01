a6f2e04b3de29b0583f57f1e2b3522293489962b
letnull19A <alexvolkovdd@googlemail.com>
2025-12-02 02:29:57 +0400

    fix(hooks): Specify type for useQuery in useAvatar hook

--- frontend/src/shared/lib/hooks/use-avatar.ts
+++ frontend/src/shared/lib/hooks/use-avatar.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { AvatarApi } from '@/shared/api';
+import type { Avatar } from '@/entities';
 
 export const useAvatar = (id: string) => {
-  return useQuery({
+  return useQuery<Avatar>({
     queryKey: ['avatar', id],
     queryFn: () => AvatarApi.getById(id),
   });
